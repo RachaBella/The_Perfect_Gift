ec6762800911d666b5a8770258d492bcb2b24f9e
mikey-pawell <mikeypawell@gmail.com>
2015-11-18 14:10:42 -0800

    login

--- The_Perfect_Gift/app/controllers/users_controller.rb
+++ The_Perfect_Gift/app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all
+		render :index 
+	end 
 
+	def new
+	    @user = User.new
+	    render :new
+  	end
+
+  	def create
+    	user_params = params.require(:user).permit(:username, :email, :password)
+    	@user = User.create(user_params)
+	    login(@user) # <-- login the user
+	    redirect_to "/users/#{@user.id}" # <-- go to show
+    end
+
+    def show
+    @user = User.find(params[:id])
+    render :show
+  end
 end
