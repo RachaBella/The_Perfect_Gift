4e2bd28f26652b6e4f36fc564fd5d086ea332751
cd20202 <cd20202@live.com>
2015-11-19 17:13:54 -0800

    updated routes comment to show current paths

--- The_Perfect_Gift/config/routes.rb
+++ The_Perfect_Gift/config/routes.rb
@@ -23,9 +23,17 @@ Rails.application.routes.draw do
 	end
 
 end
-#  new_user GET    /signup(.:format)                                               users#new
+
+
+#                  Prefix Verb   URI Pattern                                                     Controller#Action
+#                    root GET    /                                                               welcome#index
+#                 aboutus GET    /aboutus(.:format)                                              layouts#aboutus
+#                  search GET    /search(.:format)                                               welcome#search
+#             search_form GET    /search_form(.:format)                                          welcome#search_form
+#                new_user GET    /signup(.:format)                                               users#new
 #                   login GET    /login(.:format)                                                sessions#new
 #                sessions POST   /sessions(.:format)                                             sessions#create
+#                  logout POST   /logout(.:format)                                               sessions#destroy
 #     user_recipent_gifts GET    /users/:user_id/recipents/:recipent_id/gifts(.:format)          gifts#index
 #                         POST   /users/:user_id/recipents/:recipent_id/gifts(.:format)          gifts#create
 #  new_user_recipent_gift GET    /users/:user_id/recipents/:recipent_id/gifts/new(.:format)      gifts#new
@@ -50,13 +58,7 @@ end
 #                         PATCH  /users/:id(.:format)                                            users#update
 #                         PUT    /users/:id(.:format)                                            users#update
 #                         DELETE /users/:id(.:format)                                            users#destroy
-#                   gifts GET    /gifts(.:format)                                                gifts#index
-#                         POST   /gifts(.:format)                                                gifts#create
-#                new_gift GET    /gifts/new(.:format)                                            gifts#new
-#               edit_gift GET    /gifts/:id/edit(.:format)                                       gifts#edit
-#                    gift GET    /gifts/:id(.:format)                                            gifts#show
-#                         PATCH  /gifts/:id(.:format)                                            gifts#update
-#                         PUT    /gifts/:id(.:format)                                            gifts#update
-#                         DELETE /gifts/:id(.:format)                                            gifts#destroy
+
+
 
 
